21f8f21fe0527384cbca045bf756477d92f327d3
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-07-30 23:09:49 +0900

    react-wasm

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 
