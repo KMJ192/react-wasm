036670579c2d5e9127970cf42c904c4b029149ac
KMJ192 <myeongjun222@naver.com>
2021-07-30 10:52:12 +0900

    react-wasm

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
+interface WasmModule{
+  default: typeof import("../node_modules/wasm-set/wasm_rust");
+  greet(name: string): void;
+  add(a: number, b: number): number;
+  sub(a: number, b: number): number;
+  mul(a: number, b: number): number;
+  div(a: number, b: number): number;
+  rust_vec(): Int32Array;
+}
+
 function App() {
   const [count, setCount] = useState(0);
-  
+
   useEffect(() => {
     setTimeout(() => {
-      import('wasm-set').then(module => {
-        setCount(module.add(count, 1));
+      import('wasm-set').then((module: WasmModule) => {
+        if(count < 9){
+          console.log(module.rust_vec()[count]);
+          setCount(module.add(count, 1));
+        }else{
+          setCount(0);
+        }
       });
     }, 1000);
   }, [count]);
--- wasm-rust/src/lib.rs
+++ wasm-rust/src/lib.rs
@@ -30,6 +30,7 @@ pub fn div(a: i32, b: i32) -> i32{
     a / b
 }
 
-pub fn tmp() -> Vec<i32>{
+#[wasm_bindgen]
+pub fn rust_vec() -> Vec<i32>{
     vec![1, 2, 3, 4, 5, 6, 7, 8, 9]
 }
